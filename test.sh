3dae67b7a93939653811c1e2d9aff35c4ed2f4a2
satoshi ogata <ogata.yobailo.satosi@gmail.com>
2022-02-19 19:50:48 +0900

    [add] クエリが正しいかどうかを判定するシェルスクリプトを追加

---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# 設定ファイルから変数を読み込む
+. ./.config
+
+# sql ファイルからクエリを作成する
+sql1=`cat recipes/$1.sql`
+sql2=`cat answers/$1.sql`
+# レコードの順番も含めてテストするため row_number() を用いた書き方にしている
+query="select *, row_number() over () from (${sql1//;/}) as t1 except select *, row_number() over () from (${sql2//;/}) as t2 "
+
+# クエリと設定を用いて結果を取得
+result=`echo "${query}"  | psql -U $USER -d $DATABASE -h $HOST`
+
+# 結果の末尾を見て返ってきたレコード数が0の場合は正解と返す
+rows=`echo ${result} | tail -c 9`
+correct_result="(0 rows)"
+if [ "${rows}"=="${correct_result}" ]
+then
+  echo '正解!'
+else
+  echo '不正解'
+fi
+exit $?
