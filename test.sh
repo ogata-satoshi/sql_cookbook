96058957a0066a175dfda7ad7bc4f1a2d6d8cc90
satoshi ogata <ogata.yobailo.satosi@gmail.com>
2022-02-19 20:50:08 +0900

    [add] 第二引数でレコード順を考慮するかどうかを判定する機能を追加 postgrsql ではレコード順が保証されないため基本的にレコード順に関係なくテストできるようにした。

--- test.sh
+++ test.sh
@@ -5,16 +5,24 @@
 # sql ファイルからクエリを作成する
 sql1=`cat recipes/$1.sql`
 sql2=`cat answers/$1.sql`
+
 # レコードの順番も含めてテストするため row_number() を用いた書き方にしている
-query="select *, row_number() over () from (${sql1//;/}) as t1 except select *, row_number() over () from (${sql2//;/}) as t2 "
+if [ "$2" = "order" ];
+then
+  query="select *, row_number() over () from (${sql1//;/}) as t1 except select *, row_number() over () from (${sql2//;/}) as t2 "
+else
+  # ORDER BY が含まれていても対応できるようにサブクエリを使用している。
+  query="select * from (${sql1//;/}) as t1 except select * from (${sql2//;/}) as t2 "
+fi
 
 # クエリと設定を用いて結果を取得
 result=`echo "${query}"  | psql -U $USER -d $DATABASE -h $HOST`
 
 # 結果の末尾を見て返ってきたレコード数が0の場合は正解と返す
 rows=`echo ${result} | tail -c 9`
+
 correct_result="(0 rows)"
-if [ "${rows}"=="${correct_result}" ]
+if [ "${rows}" = "${correct_result}" ];
 then
   echo '正解!'
 else
