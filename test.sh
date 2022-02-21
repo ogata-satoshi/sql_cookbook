90685221199cc6b985a15ba4f320622f36ddcf4e
satoshi ogata <ogata.yobailo.satosi@gmail.com>
2022-02-21 22:11:20 +0900

    [change] テスト用のシェルスクリプトを BigQuery 対応へと変更

--- test.sh
+++ test.sh
@@ -1,31 +1,38 @@
 #!/bin/bash
-# 設定ファイルから変数を読み込む
-. ./.config
-
 # sql ファイルからクエリを作成する
 sql1=`cat recipes/$1.sql`
+if [ "${sql1}" = "" ]; then
+  echo "正解データが存在しません。"
+  exit $?
+fi
 sql2=`cat answers/$1.sql`
+if [ "${sql2}" = "" ]; then
+  echo "解答データが存在しません"
+  exit $?
+fi
+
+# * をワイルドカードとして使用しないために必要
+set -f
 
 # レコードの順番も含めてテストするため row_number() を用いた書き方にしている
 if [ "$2" = "order" ];
 then
-  query="select *, row_number() over () from (${sql1//;/}) as t1 except select *, row_number() over () from (${sql2//;/}) as t2 UNION ALL select *, row_number() over () from (${sql2//;/}) as t3 except select *, row_number() over () from (${sql1//;/}) as t4"
+  query="(select *, row_number() over () from (${sql1//;/}) as t1 except distinct select *, row_number() over () from (${sql2//;/}) as t2) UNION ALL (select *, row_number() over () from (${sql2//;/}) as t3 except distinct select *, row_number() over () from (${sql1//;/}) as t4)"
 else
   # ORDER BY が含まれていても対応できるようにサブクエリを使用している。
-  query="select * from (${sql1//;/}) as t1 except select * from (${sql2//;/}) as t2 UNION ALL select * from (${sql2//;/}) as t3 except select * from (${sql1//;/}) as t4 "
+  query="(select * from (${sql1//;/}) as t1 except distinct select * from (${sql2//;/}) as t2) UNION ALL (select * from (${sql2//;/}) as t3 except distinct select * from (${sql1//;/}) as t4)"
 fi
 
 # クエリと設定を用いて結果を取得
-result=`echo "${query}"  | psql -U $USER -d $DATABASE -h $HOST`
+result=`echo "${query}" | bq query --use_legacy_sql=false`
+echo "${result}"
 
-# 結果の末尾を見て返ってきたレコード数が0の場合は正解と返す
-rows=`echo ${result} | tail -c 9`
-
-correct_result="(0 rows)"
-if [ "${rows}" = "${correct_result}" ];
+# 結果が返ってこなければ正解とする
+rows=`echo ${result}`
+if [ "${rows}" ];
 then
-  echo '正解!'
-else
   echo '不正解'
+else
+  echo '正解!'
 fi
 exit $?
