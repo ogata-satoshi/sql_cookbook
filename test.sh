50d4acab64cb8fab9e28f2b18b5a1e879afa647b
satoshi ogata <ogata.yobailo.satosi@gmail.com>
2022-02-21 09:13:52 +0900

    [fix] テストの方法が間違っていたので修正 except で片方のみ差分をとっていたが、片方の結果がもう片方の結果の部分集合である場合に間違って正解と判定されるので修正

--- test.sh
+++ test.sh
@@ -9,10 +9,10 @@ sql2=`cat answers/$1.sql`
 # レコードの順番も含めてテストするため row_number() を用いた書き方にしている
 if [ "$2" = "order" ];
 then
-  query="select *, row_number() over () from (${sql1//;/}) as t1 except select *, row_number() over () from (${sql2//;/}) as t2 "
+  query="select *, row_number() over () from (${sql1//;/}) as t1 except select *, row_number() over () from (${sql2//;/}) as t2 UNION ALL select *, row_number() over () from (${sql2//;/}) as t3 except select *, row_number() over () from (${sql1//;/}) as t4"
 else
   # ORDER BY が含まれていても対応できるようにサブクエリを使用している。
-  query="select * from (${sql1//;/}) as t1 except select * from (${sql2//;/}) as t2 "
+  query="select * from (${sql1//;/}) as t1 except select * from (${sql2//;/}) as t2 UNION ALL select * from (${sql2//;/}) as t3 except select * from (${sql1//;/}) as t4 "
 fi
 
 # クエリと設定を用いて結果を取得
