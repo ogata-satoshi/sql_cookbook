2e967e18ab38f1b9b5806df31a8d54205f427227
satoshi ogata <ogata.yobailo.satosi@gmail.com>
2022-02-27 10:01:34 +0900

    [add] 3章で使用するテーブルを作成するクエリを追加

--- init/create_table.sql
+++ init/create_table.sql
@@ -180,6 +180,18 @@ VALUES
   (30, 'SALES', 'CHICAGO'),
   (40, 'OPERATIONS', 'BOSTON');
 
+-- 3章で使うテーブルを作成する
+DROP TABLE IF EXISTS sql_cookbook.emp_bonus;
+
+CREATE TABLE sql_cookbook.emp_bonus (EMPNO int, RECEIVED STRING, TYPE int);
+
+INSERT INTO
+  sql_cookbook.emp_bonus (EMPNO, RECEIVED, TYPE)
+VALUES
+  (7369, '14-MAR-2015', 1),
+  (7900, '14-MAR-2015', 2),
+  (7788, '14-MAR-2015', 2);
+
 -- 第5章のために SMEAGOL スキーマを作成しておく
 DROP SCHEMA IF EXISTS SMEAGOL CASCADE;
 
